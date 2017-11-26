0b9bc1a90485abb77d3f6c8fdeec9a3e83f4e381
mahmoud elgeiziry <mahmoud.geiziry@hotmail.com>
2017-11-26 15:15:12 +0300

    server project, following the pluralsight course

---
+++ mainview.cpp
@@ -0,0 +1,51 @@
+#include "mainview.h"
+#include "ui_mainview.h"
+#include <QTcpServer>
+#include <QMessageBox>
+
+static int PORT_NUMBER = 23;
+
+MainView::MainView(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainView),
+    m_server(new QTcpServer(this))
+{
+    ui->setupUi(this);
+}
+
+MainView::~MainView()
+{
+    delete ui;
+}
+
+void MainView::on_btnStartServer_clicked()
+{
+    if (StartServer()) {
+        ui->btnStartServer->setEnabled(false);
+        ui->btnStopServer->setEnabled(true);
+    }
+
+}
+
+void MainView::on_btnStopServer_clicked()
+{
+    StopServer();
+    ui->btnStartServer->setEnabled(true);
+    ui->btnStopServer->setEnabled(false);
+
+}
+
+bool MainView::StartServer(){
+    bool result = m_server->listen(QHostAddress::Any,PORT_NUMBER);
+    if (!result) {
+        QMessageBox::critical(this,tr("Echo Server"),
+                              tr("Unable to start the server: %1")
+                              .arg(m_server->errorString()));
+        return false;
+    }
+    return true;
+}
+
+void MainView::StopServer(){
+
+}
